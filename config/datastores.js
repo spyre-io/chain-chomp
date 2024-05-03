97f34b60b8eed7e6e401511eac7b5ff1938887b6
Benjamin Jordan <benjamin@thegoldenmule.com>
2024-05-02 19:59:57 -0500

    data store and migration updates

--- config/datastores.js
+++ config/datastores.js
@@ -3,5 +3,6 @@ module.exports.datastores = {
   default: {
     adapter: require('sails-postgresql'),
     url: `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/chain_chomp`,
+    ssl: process.env.POSTGRES_SSL === 'true' ? true : false,
   },
 };
--- config/models.js
+++ config/models.js
@@ -2,14 +2,13 @@
 module.exports.models = {
 
   schema: true,
-  migrate: 'alter',
+  migrate: 'safe',
 
   attributes: {
     createdAt: { type: 'number', autoCreatedAt: true, },
     updatedAt: { type: 'number', autoUpdatedAt: true, },
     id: { type: 'number', autoIncrement: true, },
   },
-
 
   dataEncryptionKeys: {
     default: 'HOOtfAPUZ6g8OoAurmJx1KOzZUfSwVmfzCUilfU9TVQ='
